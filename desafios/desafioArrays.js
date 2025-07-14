3dd19420b4029d164216950b061c1e6caf2b5a12
Isack de Melo <demelodev@gmail.com>
2025-07-14 15:21:25 -0300

    Add files via upload

---
+++ desafios/desafioArrays.js
@@ -0,0 +1,22 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+
+const listaGenerica = []; 
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+const linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+const nomes1 = ["Isack", "Juliana", "Jorge"];
+console.log(nomes1[1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+const nomes2 = ["Isack", "Juliana", "Jorge"];
+console.log(nomes2[2]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+const nomes3 = ["Isack", "Juliana", "Jorge"];
+console.log(nomes3[3]);
